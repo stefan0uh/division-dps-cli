f40d4c593edacb1c090caaa17aa719a82b44c399
Stefan <stefan.mairbaeurl@publicispixelpark.de>
2016-05-14 03:46:46 +0200

    armor berechnung eingefügt

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-
-/**
+/*
  * Created by Stefan Mairbaeurl
  */
+
 var jsonfile = require('jsonfile')
 var path = require('path')
 
@@ -10,22 +10,57 @@ var char = jsonfile.readFileSync(path.join(__dirname, 'gear.json'))
 
 var firearms = 535,
   stamina = 535,
-  electronic = 535
+  electronic = 535,
+  armor = 0,
+  crit = 0,
+  critDMG = 0
 
 for (i = 0; i < 6; i++) {
-  if (char.equip[i].firearms) {
-    firearms += char.equip[i].firearms
+  // Main Stats
+  if (char.equip[i].stats.firearms) {
+    firearms += char.equip[i].stats.firearms
   }
-  if (char.equip[i].stamina) {
-    stamina += char.equip[i].stamina
+  if (char.equip[i].stats.stamina) {
+    stamina += char.equip[i].stats.stamina
   }
-  if (char.equip[i].electronic) {
-    electronic += char.equip[i].electronic
+  if (char.equip[i].stats.electronic) {
+    electronic += char.equip[i].stats.electronic
+  }
+  if (char.equip[i].armor) {
+    armor += char.equip[i].armor
+  }
+  // Major Stats
+  if (char.equip[i].crit) {
+    crit += char.equip[i].crit
+  }
+  if (char.equip[i].critDMG) {
+    critDMG += char.equip[i].critDMG
   }
 }
 
-console.log(firearms)
-console.log(stamina)
-console.log(electronic)
+console.log('\n-- Main Stats -------')
+console.log('Firearms:', firearms)
+console.log('Stamina:', stamina)
+console.log(' > Health:', stamina * 30)
+console.log(' + Effectiv Health:', Math.round((stamina * 30) / (1 - (armor * 0.00014) / (1 + armor * 0.00014))))
+console.log(' > Armor:', armor, '(' + Math.min(Math.max(parseInt(armor * 0.0140), 1), 65) + '%)')
+console.log('\nElectronic:', electronic)
+console.log(' > Skillpower:', electronic * 10)
+console.log('\n-- Major Stats ------')
+console.log('Crit Chance:', crit + '%')
+console.log('Crit Damage:', critDMG + '%')
+console.log("\n-- Weapon's ---------")
+console.log('Primary Weapon:', char.weapon.primary.name)
+console.log(' > Magazin:', char.weapon.primary.mag, '(' + round2two((60 / char.weapon.primary.rof) * char.weapon.primary.mag) + 'sec)')
+console.log(' > Weapon Damage (PVE):', char.weapon.primary.currentDamage)
+console.log(' > Weapon Damage (PVP):', Math.round(char.weapon.primary.currentDamage * 0.4))
+console.log('\nSecondary Weapon:', char.weapon.secondary.name)
+console.log(' > Magazin:', char.weapon.secondary.mag, '(' + round2two((60 / char.weapon.secondary.rof) * char.weapon.secondary.mag) + 'sec)')
+console.log(' > Weapon Damage (PVE):', char.weapon.secondary.currentDamage)
+console.log(' > Weapon Damage (PVP):', Math.round(char.weapon.secondary.currentDamage * 0.4))
+console.log('\nSidearm Weapon:', char.weapon.sidearm.name)
+console.log(' > Magazin:', char.weapon.sidearm.mag, '(' + round2two((60 / char.weapon.sidearm.rof) * char.weapon.sidearm.mag) + 'sec)')
+console.log(' > Weapon Damage (PVE):', char.weapon.sidearm.currentDamage)
+console.log(' > Weapon Damage (PVP):', Math.round(char.weapon.sidearm.currentDamage * 0.4))
 
-// var powerlevelCalc = ((weaponBullet * 100) / weaponBasicDmg - firearms) / weaponCondition
+function round2two (x) { Ergebnis = Math.round(x * 100) / 100 ; return Ergebnis; }
