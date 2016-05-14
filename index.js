dcf2887c508701c4111f166664c882fbe4f9085b
Stefan <stefan.mairbaeurl@publicispixelpark.de>
2016-05-14 02:22:28 +0200

    equip einlesen

--- index.js
+++ index.js
@@ -3,7 +3,29 @@
 /**
  * Created by Stefan Mairbaeurl
  */
+var jsonfile = require('jsonfile')
+var path = require('path')
 
-var dps = process.argv[2]
+var char = jsonfile.readFileSync(path.join(__dirname, 'gear.json'))
 
-console.log(dps)
+var firearms = 535,
+  stamina = 535,
+  electronic = 535
+
+for (i = 0; i < 6; i++) {
+  if (char.equip[i].firearms) {
+    firearms += char.equip[i].firearms
+  }
+  if (char.equip[i].stamina) {
+    stamina += char.equip[i].stamina
+  }
+  if (char.equip[i].electronic) {
+    electronic += char.equip[i].electronic
+  }
+}
+
+console.log(firearms)
+console.log(stamina)
+console.log(electronic)
+
+// var powerlevelCalc = ((weaponBullet * 100) / weaponBasicDmg - firearms) / weaponCondition
